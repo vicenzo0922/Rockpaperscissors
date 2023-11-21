6024d9109688a08fc476d52cde05143550e97243
vicenzo0922 <vicenzograal@gmail.com>
2023-11-21 21:57:12 +0800

    update

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
+let userScore = 0;
+let computerScore = 0;
+
+function updateScores(){
+document.querySelector('#userScore').innerHTML = userScore;
+document.querySelector('#computerScore').innerHTML= computerScore;
+}
+
+
+
 document.querySelector('#rockBtn').addEventListener('click', ()=>{
     selectChoice('rock');
 });
@@ -9,10 +19,49 @@ document.querySelector('#scissorsBtn').addEventListener('click', ()=>{
 });
 
 function selectChoice(choice) {
-    alert(choice);
+    const computerChoice = makeComputerChoice();
+
+    if(choice === computerChoice){
+        alert('DRAW')
+    }
+
+    if(choice === 'rock'){
+        if(computerChoice === 'paper'){
+            computerScore++;
+            alert('Computer Wins');
+        }if(computerChoice === 'scissors'){
+            userScore++;
+            alert('You Win');
+        }
+    }
+
+    if(choice === 'paper'){
+        if(computerChoice === 'scissors'){
+            computerScore++;
+            alert('Computer Wins');
+        }if(computerChoice === 'rock'){
+            userScore++;
+            alert('You Win');
+        }
+    }
+
+    if(choice === 'scissors'){
+        if(computerChoice === 'rock'){
+            computerScore++;
+            alert('Computer Wins');
+        }if(computerChoice === 'paper'){
+            userScore++;
+            alert('You Win');
+        }
+    }
+
+
+
+    updateScores();
 }
 
 function makeComputerChoice(){
     const choices = ['rock', 'paper', 'scissors'];
-    Math.floor(Math.random() * choices.length);
+    const choice = choices[Math.floor(Math.random() * choices.length)];
+    return choice;
 }
